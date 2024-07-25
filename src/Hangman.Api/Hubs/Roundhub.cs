1cd4d06146d8ef6b409af2cef104692f86e662b1
aikhae <me@eike.in>
2024-07-25 18:52:14 +0200

    Converting roomCodes to uppercase

--- src/Hangman.Api/Hubs/Roundhub.cs
+++ src/Hangman.Api/Hubs/Roundhub.cs
@@ -22,45 +22,45 @@ namespace Hangman.Api.Hubs
         public async Task MakeGuess(string userId, string guess, string roomCode, string round, IGameStateService gameStateService)
         {
             CreateGuessRequest guessReqObj = new CreateGuessRequest { guess = guess };
-            var guessObj = guessReqObj.MapToGuess(roomCode, Guid.Parse(userId), Int32.Parse(round));
+            var guessObj = guessReqObj.MapToGuess(roomCode.ToUpper(), Guid.Parse(userId), Int32.Parse(round));
             guessObj = await gameStateService.HandleGuess(guessObj);
-            var roundState = await gameStateService.GetRoundStatus(new RoundStatus { roomCode = roomCode, roundNum = Int32.Parse(round), userId = Guid.Parse(userId) });
-            await Clients.Group(roomCode).SendAsync("GameState", roundState.MapToResponse());
+            var roundState = await gameStateService.GetRoundStatus(new RoundStatus { roomCode = roomCode.ToUpper(), roundNum = Int32.Parse(round), userId = Guid.Parse(userId) });
+            await Clients.Group(roomCode.ToUpper()).SendAsync("GameState", roundState.MapToResponse());
         }
 
         public async Task JoinGame(string roomCode, string nickname, IGameService gameService, IUserService userService)
         {
-            Player playerObject = new Player { Id = Guid.NewGuid(), Nickname = nickname, roomCode = roomCode };
+            Player playerObject = new Player { Id = Guid.NewGuid(), Nickname = nickname, roomCode = roomCode.ToUpper() };
             var player = await gameService.JoinGameAsync(playerObject);
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, roomCode);
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomCode.ToUpper());
 
             // Adding the user guid to identity to identify user on disconnect
             var claims = new List<Claim>();
             claims.Add(new Claim(ClaimTypes.Name, playerObject.Id.ToString()));
-            claims.Add(new Claim(ClaimTypes.GroupSid, roomCode));
+            claims.Add(new Claim(ClaimTypes.GroupSid, roomCode.ToUpper()));
             var identity = new ClaimsIdentity(claims);
             Context.User!.AddIdentity(identity);
 
             await Clients.Caller.SendAsync("PlayerData", playerObject.MapToResponse());
             
-            var players = await gameService.GetAllPlayers(roomCode, playerObject.Id);
-            bool isGameLeader = await userService.IsUserGameLeader(roomCode, playerObject.Id);
+            var players = await gameService.GetAllPlayers(roomCode.ToUpper(), playerObject.Id);
+            bool isGameLeader = await userService.IsUserGameLeader(roomCode.ToUpper(), playerObject.Id);
 
-            await Clients.Group(roomCode).SendAsync("NewPlayer", players.MapToResponse(isGameLeader));
+            await Clients.Group(roomCode.ToUpper()).SendAsync("NewPlayer", players.MapToResponse(isGameLeader));
         }
 
         public async Task StartGame(string roomCode, Guid userid, IGameService gameService)
         {
-            var round = await gameService.StartGameAsync(roomCode, userid);
-            await Clients.Group(roomCode).SendAsync("GameStarted", round.MapToResponse());
+            var round = await gameService.StartGameAsync(roomCode.ToUpper(), userid);
+            await Clients.Group(roomCode.ToUpper()).SendAsync("GameStarted", round.MapToResponse());
         }
 
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             await base.OnDisconnectedAsync(exception);
             Guid userId = Guid.Parse(Context.User!.Identities.ToList()[1].Name!);
-            string roomCode = Context.User!.Identities.ToList()[1].Claims.ToList()[1].ToString().Split(": ")[1];
+            string roomCode = Context.User!.Identities.ToList()[1].Claims.ToList()[1].ToString().Split(": ")[1].ToUpper();
 
             // Remove player
             await _userService.RemovePlayer(roomCode, userId);
